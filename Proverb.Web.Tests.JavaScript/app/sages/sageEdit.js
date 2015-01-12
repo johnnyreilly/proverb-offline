bd2a93ff9ef52ebbe2abf906c743f73ddb8a3234
John Reilly <johnny_reilly@hotmail.com>
2015-01-12 17:20:15 +0000

    refactor approach + more tests

--- Proverb.Web.Tests.JavaScript/app/sages/sageEdit.js
+++ Proverb.Web.Tests.JavaScript/app/sages/sageEdit.js
@@ -10,13 +10,14 @@
         var $rootScope,
             getById_deferred, // deferred used for promises
             $location, $scope, $routeParams_stub, common, datacontext, // controller dependencies
-            sageEditController; // the controller
+            $controller, sageEditController; // the controller
 
         beforeEach(inject(function (_$controller_, _$location_, _$rootScope_, _$q_, _common_, _datacontext_) {
 
             $rootScope = _$rootScope_;
             $q = _$q_;
 
+            $controller = _$controller_;
             $location = _$location_;
             $scope = $rootScope.$new();
             common = _common_;
@@ -30,18 +31,27 @@
             spyOn(common.logger, "getLoggers").and.returnValue({
                 info: jasmine.createSpy("logInfo")
             });
-            spyOn($location, "path")/*.and.returnValue(jasmine.createSpy("path"))*/;
+        }));
 
-            sageEditController = _$controller_("sageEdit", {
+        /**
+         * Construct a new controller
+         */
+        function makeController() {
+            return $controller("sageEdit", {
                 $location: $location,
                 $scope: $scope,
                 $routeParams: $routeParams_stub,
                 common: common,
                 datacontext: datacontext
             });
-        }));
+        }
 
         describe("on creation ->", function () {
+
+            beforeEach(function () {
+
+                sageEditController = makeController();
+            });
 
             it("controller should have a title of 'Sage Edit'", function () {
 
@@ -63,7 +73,10 @@
 
             var sage_stub;
             beforeEach(function () {
+
                 sage_stub = { name: "John" };
+
+                sageEditController = makeController();
             });
 
             it("should set sage to be the resolved promise values", function () {
@@ -84,21 +97,56 @@
             });
         });
 
-        //describe("gotoEdit ->", function () {
-        //
-        //    var sage_stub;
-        //    beforeEach(function () {
-        //        sage_stub = { id: 20 };
-        //    });
-        //
-        //    it("should set $location.path to edit URL with the sage id", function () {
-        //
-        //        sageEditController.sage = sage_stub;
-        //
-        //        sageEditController.gotoEdit();
-        //
-        //        expect($location.path).toHaveBeenCalledWith("/sages/edit/" + sage_stub.id);
-        //    });
-        //});
+        describe("save ->", function () {
+        
+            var sage_stub, save_deferred;
+            beforeEach(function () {
+                sage_stub = { id: 20, name: "John" };
+
+                save_deferred = $q.defer();
+
+                spyOn(datacontext.sage, "save").and.returnValue(save_deferred.promise);
+                spyOn(common, "waiter").and.callThrough();
+                spyOn($location, "path");
+
+                sageEditController = makeController();
+                sageEditController.sage = sage_stub;
+            });
+
+            it("_isSavingOrRemoving should be set true", function () {
+
+                expect(sageEditController._isSavingOrRemoving).toBe(false);
+                sageEditController.save();
+                expect(sageEditController._isSavingOrRemoving).toBe(true);
+            });
+
+            it("serverErrors should be wiped", function () {
+
+                sageEditController.errors = { "errors": "aplenty" };
+                sageEditController.save();
+                expect(sageEditController.errors).toEqual({});
+            });
+
+            it("datacontext.sage.save should be called", function () {
+
+                sageEditController.save();
+                expect(datacontext.sage.save).toHaveBeenCalledWith(sage_stub);
+            });
+
+            it("common.waiter should be called", function () {
+
+                sageEditController.save();
+                expect(common.waiter).toHaveBeenCalledWith(save_deferred.promise, "sageEdit", "Saving " + sage_stub.name);
+            });
+
+            xit("should set $location.path to edit URL with the sage id", function () {
+        
+                sageEditController.sage = sage_stub;
+        
+                sageEditController.save();
+        
+                expect($location.path).toHaveBeenCalledWith("/sages/detail/" + sage_stub.id);
+            });
+        });
     });
 });
