1667db3a7c29ceeaa509e50e11cc2c5463ddbb36
John Reilly <johnny_reilly@hotmail.com>
2015-02-03 10:35:00 +0000

    Split out config into gulpfile.config and moved index.html to be published as part of the app

---
+++ Proverb.Web/gulpfile.config.js
@@ -0,0 +1,76 @@
+﻿var tsjsmapjsSuffix = ".{ts,js.map,js}";
+
+var bower = "bower_components/";
+var app = "app/";
+
+var config = {
+
+    base: ".",
+    buildDir: "./build/",
+    debug: "debug",
+    release: "release",
+    css: "css",
+
+    bundleNames: { scripts: "scripts", styles: "styles" },
+
+    bootFile: app + "index.html",
+    bootjQuery: bower + "jquery/dist/jquery.min.js",
+
+    // The fonts we want Gulp to process
+    fonts: [bower + "fontawesome/fonts/*.*"],
+
+    // The scripts we want Gulp to process
+    scripts: [
+        // Vendor Scripts 
+        bower + "angular/angular.js",
+        bower + "angular-animate/angular-animate.js",
+        bower + "angular-route/angular-route.js",
+        bower + "angular-sanitize/angular-sanitize.js",
+        bower + "angular-bootstrap/ui-bootstrap-tpls.js",
+
+        bower + "toastr/toastr.js",
+        bower + "moment/moment.js",
+        "scripts/spin.js",
+        bower + "underscore/underscore.js",
+
+        // Bootstrapping
+        app + "app" + tsjsmapjsSuffix,
+        app + "config.route" + tsjsmapjsSuffix,
+
+        // common Modules
+        app + "common/common" + tsjsmapjsSuffix,
+        app + "common/logger" + tsjsmapjsSuffix,
+        app + "common/spinner" + tsjsmapjsSuffix,
+
+        // common.bootstrap Modules
+        app + "common/bootstrap/bootstrap.dialog" + tsjsmapjsSuffix,
+
+        // directives
+        app + "directives/**/*" + tsjsmapjsSuffix,
+
+        // services
+        app + "services/**/*" + tsjsmapjsSuffix,
+
+        // controllers
+        app + "about/**/*" + tsjsmapjsSuffix,
+        app + "admin/**/*" + tsjsmapjsSuffix,
+        app + "dashboard/**/*" + tsjsmapjsSuffix,
+        app + "layout/**/*" + tsjsmapjsSuffix,
+        app + "sages/**/*" + tsjsmapjsSuffix,
+        app + "sayings/**/*" + tsjsmapjsSuffix
+    ],
+
+    // The styles we want Gulp to process
+    styles: [
+        "content/ie10mobile.css",
+        bower + "bootstrap/dist/css/bootstrap.css",
+        bower + "fontawesome/css/font-awesome.css",
+        bower + "toastr/toastr.css",
+        "content/styles.css"
+    ]
+};
+
+config.debugFolder = config.buildDir + config.debug + "/";
+config.releaseFolder = config.buildDir + config.release + "/";
+
+module.exports = config;
--- Proverb.Web/gulpfile.js
+++ Proverb.Web/gulpfile.js
@@ -14,79 +14,8 @@ var templateCache = require("gulp-angular-templatecache");
 var eventStream = require("event-stream");
 var order = require("gulp-order");
 var gulpUtil = require("gulp-util");
-//var sourcemaps = require('gulp-sourcemaps');
 
-//var tsjsmapjsSuffix = ".js";
-var tsjsmapjsSuffix = ".{ts,js.map,js}";
-var excludetsjsmap = "**/*.{ts,js.map}";
-
-var bundleNames = { scripts: "scripts", styles: "styles" };
-
-var paths = {
-
-    base: ".",
-    buildDir: "./build/",
-    debug: "debug",
-    release: "release",
-    css: "css",
-
-    // The fonts we want Gulp to process
-    fonts: ["bower_components/fontawesome/fonts/*.*"],
-
-    // The scripts we want Gulp to process
-    scripts: [
-        // Vendor Scripts 
-        "bower_components/angular/angular.js",
-        "bower_components/angular-animate/angular-animate.js",
-        "bower_components/angular-route/angular-route.js",
-        "bower_components/angular-sanitize/angular-sanitize.js",
-        "bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
-
-        "bower_components/toastr/toastr.js",
-        "bower_components/moment/moment.js",
-        "scripts/spin.js",
-        "bower_components/underscore/underscore.js",
-
-        // Bootstrapping
-        "app/app" + tsjsmapjsSuffix,
-        "app/config.route" + tsjsmapjsSuffix,
-
-        // common Modules
-        "app/common/common" + tsjsmapjsSuffix,
-        "app/common/logger" + tsjsmapjsSuffix,
-        "app/common/spinner" + tsjsmapjsSuffix,
-
-        // common.bootstrap Modules
-        "app/common/bootstrap/bootstrap.dialog" + tsjsmapjsSuffix,
-
-        // directives
-        "app/directives/**/*" + tsjsmapjsSuffix,
-
-        // services
-        "app/services/**/*" + tsjsmapjsSuffix,
-
-        // controllers
-        "app/about/**/*" + tsjsmapjsSuffix,
-        "app/admin/**/*" + tsjsmapjsSuffix,
-        "app/dashboard/**/*" + tsjsmapjsSuffix,
-        "app/layout/**/*" + tsjsmapjsSuffix,
-        "app/sages/**/*" + tsjsmapjsSuffix,
-        "app/sayings/**/*" + tsjsmapjsSuffix
-    ],
-
-    // The styles we want Gulp to process
-    styles: [
-        "content/ie10mobile.css",
-        "bower_components/bootstrap/dist/css/bootstrap.css",
-        "bower_components/fontawesome/css/font-awesome.css",
-        "bower_components/toastr/toastr.css",
-        "content/styles.css"
-    ]
-};
-
-paths.debugFolder = paths.buildDir + paths.debug + "/";
-paths.releaseFolder = paths.buildDir + paths.release + "/";
-
+var config = require("./gulpfile.config.js");
 
 /**
  * Create a manifest depending upon the supplied arguments
@@ -97,7 +26,7 @@ paths.releaseFolder = paths.buildDir + paths.release + "/";
 function getManifest(manifestName, bundleName) {
 
     // Determine filename ("./build/manifest-debug.json" or "./build/manifest-release.json"
-    var manifestFile = paths.buildDir + "manifest-" + manifestName + ".json";
+    var manifestFile = config.buildDir + "manifest-" + manifestName + ".json";
 
     gulpUtil.log("Creating " + bundleName + " manifest: " + manifestFile);
 
@@ -118,10 +47,14 @@ function getManifest(manifestName, bundleName) {
 function getScriptsAndTemplates(isDebug) {
 
     //Get the view templates for $templateCache
-    var templates = gulp.src("app/**/*.html").pipe(templateCache({ module: "app", root: "app/" }));
+    var templates = gulp.src([
+            "app/**/*.html",
+            "!app/index.html" // Exclude the launch page
+        ])
+        .pipe(templateCache({ module: "app", root: "app/" }));
 
-    var options = isDebug ? { base: paths.base } : undefined;
-    var scripts = gulp.src(paths.scripts, options);
+    var options = isDebug ? { base: config.base } : undefined;
+    var scripts = gulp.src(config.scripts, options);
 
     var combined = eventStream.merge(scripts, templates);
 
@@ -131,50 +64,48 @@ function getScriptsAndTemplates(isDebug) {
 // Delete the build folder
 gulp.task("clean", function (cb) {
 
-    return del([paths.buildDir], cb);
+    return del([config.buildDir], cb);
 });
 
 // Create a manifest.json for the debug build - this should have lots of script files in
 gulp.task("boot-dependencies", ["clean"], function () {
 
     return gulp
-        .src("bower_components/jquery/dist/jquery.min.js")
-        .pipe(gulp.dest(paths.buildDir));
+        .src([config.bootFile, config.bootjQuery])
+        .pipe(gulp.dest(config.buildDir));
 });
 
-// Copy across all files in paths.scripts to build/debug
+// Copy across all files in config.scripts to build/debug
 gulp.task("scripts-debug", ["clean"], function () {
 
     return getScriptsAndTemplates(true)
-        //.pipe(sourcemaps.init({ loadMaps: true, debug: true }))
-        //.pipe(sourcemaps.write('maps', { includeContent: true, sourceRoot: "/app" }))
-        .pipe(gulp.dest(paths.debugFolder));
+        .pipe(gulp.dest(config.debugFolder));
 });
 
 // Create a manifest.json for the debug build - this should have lots of script files in
 gulp.task("manifest-scripts-debug", ["scripts-debug"], function () {
 
     return gulp
-        .src(paths.debugFolder + "**/*.js")
-        .pipe(order(paths.scripts)) // templates.js is not in paths.scripts and so this will be the last script (which is fine)
-        .pipe(getManifest(paths.debug, bundleNames.scripts));
+        .src(config.debugFolder + "**/*.js")
+        .pipe(order(config.scripts)) // templates.js is not in config.scripts and so this will be the last script (which is fine)
+        .pipe(getManifest(config.debug, config.bundleNames.scripts));
 });
 
-// Copy across all files in paths.styles to build/debug
+// Copy across all files in config.styles to build/debug
 gulp.task("styles-debug", ["clean"], function () {
 
     return gulp
-        .src(paths.styles, { base: paths.base })
-        .pipe(gulp.dest(paths.debugFolder));
+        .src(config.styles, { base: config.base })
+        .pipe(gulp.dest(config.debugFolder));
 });
 
 // Create a manifest.json for the debug build - this should have lots of style files in
 gulp.task("manifest-styles-debug", ["styles-debug", "manifest-scripts-debug"], function () {
 
     return gulp
-        .src(paths.debugFolder + "**/*.css*")
-        .pipe(order(paths.styles))
-        .pipe(getManifest(paths.debug, bundleNames.styles));
+        .src(config.debugFolder + "**/*.css*")
+        .pipe(order(config.styles))
+        .pipe(getManifest(config.debug, config.bundleNames.styles));
 });
 
 // Concatenate & Minify JS for release into a single file
@@ -185,50 +116,50 @@ gulp.task("scripts-release", ["clean"], function () {
         .pipe(concat("app.js"))                          // Make a single file                                                         
         .pipe(uglify())                                  // Make the file titchy tiny small
         .pipe(rev())                                     // Suffix a version number to it
-        .pipe(gulp.dest(paths.releaseFolder)); // Write single versioned file to build/release folder
+        .pipe(gulp.dest(config.releaseFolder)); // Write single versioned file to build/release folder
 });
 
 // Create a manifest.json for the release build - this should just have a single file for scripts
 gulp.task("manifest-scripts-release", ["scripts-release"], function () {
 
     return gulp
-        .src(paths.releaseFolder + "**/*.js") // Should only be 1 file
-        .pipe(getManifest(paths.release, bundleNames.scripts));
+        .src(config.releaseFolder + "**/*.js") // Should only be 1 file
+        .pipe(getManifest(config.release, config.bundleNames.scripts));
 });
 
-// Copy across all files in paths.styles to build/debug
+// Copy across all files in config.styles to build/debug
 gulp.task("styles-release", ["clean"], function () {
 
     return gulp
-        .src(paths.styles)
+        .src(config.styles)
         .pipe(concat("app.css"))          // Make a single file
         .pipe(minifyCss())                // Make the file titchy tiny small
         .pipe(rev())                      // Suffix a version number to it
-        .pipe(gulp.dest(paths.releaseFolder + "/" + paths.css)); // Write single versioned file to build/release folder
+        .pipe(gulp.dest(config.releaseFolder + "/" + config.css)); // Write single versioned file to build/release folder
 });
 
 // Create a manifest.json for the debug build - this should have a single style files in
 gulp.task("manifest-styles-release", ["styles-release", "manifest-scripts-release"], function () {
 
     return gulp
-        .src(paths.releaseFolder + "**/*.css") // Should only be 1 file
-        .pipe(getManifest(paths.release, bundleNames.styles));
+        .src(config.releaseFolder + "**/*.css") // Should only be 1 file
+        .pipe(getManifest(config.release, config.bundleNames.styles));
 });
 
-// Copy across all fonts in paths.fonts to both release and debug locations
+// Copy across all fonts in config.fonts to both release and debug locations
 gulp.task("fonts-debug", ["clean"], function () {
 
     return gulp
-        .src(paths.fonts, { base: paths.base })
-        .pipe(gulp.dest(paths.debugFolder));
+        .src(config.fonts, { base: config.base })
+        .pipe(gulp.dest(config.debugFolder));
 });
 
-// Copy across all fonts in paths.fonts to both release and debug locations
+// Copy across all fonts in config.fonts to both release and debug locations
 gulp.task("fonts-release", ["clean"], function () {
 
     return gulp
-        .src(paths.fonts)
-        .pipe(gulp.dest(paths.releaseFolder + "/fonts"));
+        .src(config.fonts)
+        .pipe(gulp.dest(config.releaseFolder + "/fonts"));
 });
 
 // Default Task
